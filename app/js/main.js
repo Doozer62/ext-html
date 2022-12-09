171385b718b929e78b74d27625474aca888c83c2
Doozer62 <a.a.kopiev@mail.ru>
2022-12-09 16:09:43 +0300

    Edit dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,23 @@
 //R I G H T  S I D E
 document.getElementById('contacts-link').addEventListener('click', () => {
     document.querySelector('.rightside-menu').classList.add('active');
-    document.querySelector('.rightside-menu__close').classList.add('rightside-menu__close-active');
   });
 
-  
   document.querySelector('.rightside-menu__close').addEventListener('click', () => {
     document.querySelector('.rightside-menu').classList.remove('active');
-    document.querySelector('.rightside-menu__close').classList.remove('close-menu-active')
   });
     
+  //D R O P  D O W N
+  document.querySelector('.drop-btn').addEventListener('click', () => {
+    document.querySelector('.dropdown-content').classList.add('active');
+  });
 
+  document.addEventListener("click", function(e) {
+    let m = document.getElementById('dropdown-content');
+    if (e.target.id != 'drop-btn' && e.target.id != 'dropdown-content') {
+      m.style.display = 'none';
+    } else if (e.target.id == 'drop-btn') {
+      m.style.display = (m.style.display != 'block') ? 'block' : 'none';
+    }
+  });
 
